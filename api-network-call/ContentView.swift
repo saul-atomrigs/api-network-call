d5efde6b3a79b560fb60f8c4b6167ffbaec45615
Saul Lee <saul@atomrigs.io>
2023-07-06 12:38:53 +0900

    Initial Commit

---
+++ api-network-call/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  api-network-call
+//
+//  Created by 아톰릭스랩 on 2023/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ api-network-call/api_network_callApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_network_callApp.swift
+//  api-network-call
+//
+//  Created by 아톰릭스랩 on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct api_network_callApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
